ebf195241b90cfaa8952c0f8dbcf7412aa902ee6
Choi Sumin <rev3kr@gmail.com>
2025-04-17 20:54:35 +0900

    250417 20:54

--- Bronze/B5_25083.py
+++ Bronze/B5_25083.py
@@ -1,6 +1,4 @@
-print("         ,r'\"7")
-print("r`-_   ,'  ,/")
-print(" \. \". L_r'")
-print("   `~\/")
-print("      |")
-print("      |")
+lst1 = [1,1,2,2,2,8]
+lst2 = list(map(int, input().split()))
+for i in range(6):
+    print(lst1[i]-lst2[i], end = " ")
