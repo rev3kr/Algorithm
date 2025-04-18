4f66cf4c84bc31a6e7d4147832f087dc72d5fe99
Choi Sumin <rev3kr@gmail.com>
2025-04-18 12:11:38 +0900

    250418 12:07

--- Bronze/B3_2738.py
+++ Bronze/B3_2738.py
@@ -1,2 +1,12 @@
-n,m,*a=map(int,open(0).read().split())
-print(a)
+n, m = map(int, input().split())
+lst = []
+lst2 = []
+for _ in range(n):
+    lst.append(list(map(int, input().split())))
+for _ in range(n):
+    lst2.append(list(map(int, input().split())))
+for i in range(n):
+    for j in range(m):
+        lst2[i][j] += lst[i][j]
+for i in range(n):
+    print(*lst2[i])
