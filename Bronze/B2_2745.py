50f04e385a37b889aee979c022cafd029bf4c048
Choi Sumin <rev3kr@gmail.com>
2025-04-18 13:18:20 +0900

    250418 13:15

--- Bronze/B2_2745.py
+++ Bronze/B2_2745.py
@@ -1,9 +1,2 @@
 n, b = map(str, input().split())
-b = int(b)
-temp = 0
-for i in range(1, len(n)+1):
-    if 'A' <= n[i-1] <= 'Z':
-        temp += b**(len(n)-i)*(ord(n[i-1])-55)
-    else:
-        temp += b**(len(n)-i)*(int(n[i-1]))
-print(temp)
+print(int(n, int(b)))
