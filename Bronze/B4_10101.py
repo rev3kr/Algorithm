3cbab205245fd6926c9ade5a49e90f35eb66d599
rev3kr <rev3kr@gmail.com>
2025-04-19 16:32:15 +0900

    250419 16:32

---
+++ Bronze/B4_10101.py
@@ -0,0 +1,6 @@
+lst = list(map(int, open(0).read().split()))
+lst.sort()
+if lst[0] == lst[1] == lst[2] == 60: print("Equilateral")
+elif sum(lst) == 180 and (lst[0] == lst[1] or lst[1] == lst[2]): print("Isosceles")
+elif sum(lst) == 180: print("Scalene")
+else: print("Error")
