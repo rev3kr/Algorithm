3c118b106039d5f918b7322c43bb61e66cefefec
Choi Sumin <rev3kr@gmail.com>
2025-04-25 15:01:32 +0900

    250425 15:01

--- Silver/S4_1764.py
+++ Silver/S4_1764.py
@@ -1,8 +1,7 @@
 n, m = map(int, input().split())
 s1 = set([input() for _ in range(n)])
 s2 = set([input() for _ in range(m)])
-common = list(s1 & s2)
-common.sort()
+common = sorted(list(s1 & s2))
 print(len(common))
 for i in common:
     print(i)
