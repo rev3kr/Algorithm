3b8ce9ec729ca762c84edf219c8145d98bcf2ac1
Choi Sumin <rev3kr@gmail.com>
2025-05-04 12:20:25 +0900

    250504 12:20

--- Silver/S3_20920.py
+++ Silver/S3_20920.py
@@ -9,9 +9,7 @@ def execute():
         if s not in dic: dic[s] = 1
         else: dic[s] += 1
     sorted_s = dict(sorted(dic.items(), key=lambda x: x[0]))
-    print(sorted_s)
     sorted_s = dict(sorted(sorted_s.items(), key=lambda item: (item[1], len(item[0])), reverse=True))
-    print(sorted_s)
     for k in sorted_s.keys():
         print(k)
 execute()
