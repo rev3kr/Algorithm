bd08a2873c78f48dcf363c7e2fa77a516e6d1ffa
rev3kr <rev3kr@gmail.com>
2025-06-28 17:52:58 +0900

    250628 17:52

---
+++ Silver/S2_1874.py
@@ -0,0 +1,25 @@
+def execute():
+    n = int(input())
+    string = ""
+    stack = []
+    num = temp = float('inf')
+    for i in range(1, n+1):
+        value = int(input())
+        if i == 1:
+            for j in range(1, value+1):
+                stack.append(j)
+                string += "+"
+            num = value
+        if value > num:
+            for j in range(num+1, value+1):
+                stack.append(j)
+                string += "+"
+            num = value
+        if value <= num:
+            temp = stack.pop()
+            string += "-"
+            if temp != value:
+                print("NO")
+                return
+    print('\n'.join(string))
+execute()
