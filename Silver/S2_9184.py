5a2cbf19c3b07f84da773e0eeebb2134fea0f49c
rev3kr <rev3kr@gmail.com>
2025-07-05 17:23:27 +0900

    250705 17:23

--- Silver/S2_9184.py
+++ Silver/S2_9184.py
@@ -1,18 +1,20 @@
 import sys
 input = sys.stdin.readline
-def execute():
+def main():
+    w = [[[1 for _ in range(21)] for _ in range(21)] for _ in range(21)]
+    for i in range(1, 21):
+        for j in range(1, 21):
+            for k in range(1, 21):
+                if i < j < k:
+                    w[i][j][k] = w[i][j][k-1] + w[i][j-1][k-1] - w[i][j-1][k]
+                else:
+                    w[i][j][k] = w[i-1][j][k] + w[i-1][j-1][k] + w[i-1][j][k-1] - w[i-1][j-1][k-1]
     while True:
         a, b, c = map(int, input().split())
         if a == b == c == -1: break
-        w = [[[1 for _ in range(21)] for _ in range(21)] for _ in range(21)]
-        for i in range(1, 21):
-            for j in range(1, 21):
-                for k in range(1, 21):
-                    if i < j < k:
-                        w[i][j][k] = w[i][j][k-1] + w[i][j-1][k-1] - w[i][j-1][k]
-                    else:
-                        w[i][j][k] = w[i-1][j][k] + w[i-1][j-1][k] + w[i-1][j][k-1] - w[i-1][j-1][k-1]
         if a <= 0 or b <= 0 or c <= 0: print(f"w({a}, {b}, {c}) = {w[0][0][0]}")
         elif a > 20 or b > 20 or c > 20: print(f"w({a}, {b}, {c}) = {w[20][20][20]}")
         else: print(f"w({a}, {b}, {c}) = {w[a][b][c]}")
-execute()
+
+if __name__ == "__main__":
+    main()
